bdda204a2ac05348492434635b82d29783f27d9d
Ankita Sawant <ankitasawant@Ankitas-MBP.attlocal.net>
2019-06-20 22:09:12 -0700

    Employee_database

---
+++ Employee_table_schemata_db.sql
@@ -0,0 +1,80 @@
+------------------DATA ENGINEERING-----------------
+
+------DROP TABLES------
+DROP TABLE departments;
+DROP TABLE dept_emp;
+DROP TABLE dept_manager;
+DROP TABLE employees;
+DROP TABLE salaries;
+DROP TABLE titles;
+
+---Create Tables and import the csv files into the coressponding tables
+CREATE TABLE departments (
+    dept_no VARCHAR  NOT NULL ,
+    dept_name VARCHAR  NOT NULL ,
+    CONSTRAINT PK_departments PRIMARY KEY(dept_no)
+);
+
+CREATE TABLE dept_emp (
+    emp_no INT  NOT NULL ,
+    dept_no VARCHAR  NOT NULL ,
+    from_date DATE  NOT NULL ,
+    to_date DATE  NOT NULL 
+);
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR  NOT NULL ,
+    emp_no INT  NOT NULL ,
+    from_date DATE  NOT NULL ,
+    to_date DATE  NOT NULL 
+);
+
+CREATE TABLE employees (
+    emp_no INT  NOT NULL ,
+    birth_date DATE  NOT NULL ,
+    first_name VARCHAR  NOT NULL ,
+    last_name VARCHAR  NOT NULL ,
+    gender VARCHAR  NOT NULL ,
+    hire_date DATE  NOT NULL ,
+    CONSTRAINT PK_employees PRIMARY KEY(emp_no)
+);
+
+CREATE TABLE salaries (
+    emp_no INT  NOT NULL ,
+    salary INT  NOT NULL ,
+    from_date DATE  NOT NULL ,
+    to_date DATE  NOT NULL 
+);
+
+CREATE TABLE titles (
+    emp_no INT  NOT NULL ,
+    title VARCHAR  NOT NULL ,
+    from_date DATE  NOT NULL ,
+    to_date DATE  NOT NULL 
+);
+
+ALTER TABLE dept_emp ADD CONSTRAINT FK_dept_emp_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
+ALTER TABLE dept_emp ADD CONSTRAINT FK_dept_emp_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
+
+ALTER TABLE dept_manager ADD CONSTRAINT FK_dept_manager_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
+
+ALTER TABLE dept_manager ADD CONSTRAINT FK_dept_manager_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
+ALTER TABLE salaries ADD CONSTRAINT FK_salaries_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
+ALTER TABLE titles ADD CONSTRAINT FK_titles_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
+---Confirming the data in the table
+SELECT * FROM departments; 
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
